5ce7e8325b296e395b23a7f42e5dbef935fffd16
gcoleman2001 <gcoleman2001@users.noreply.github.com>
2018-04-01 16:03:58 +0100

    Updated with commit

--- ExcelImport.py
+++ ExcelImport.py
@@ -3,3 +3,5 @@ import xlrd
 workbook = xlrd.open_workbook('C:\\Users\\Admin\\Desktop\\TestExcelImport.xlsx')
 worksheet = workbook.sheet_by_name('Sheet1')
 print(worksheet.cell(0,0).value)
+print(worksheet.cell(1,0).value)
+print(worksheet.cell(2,0).value)
